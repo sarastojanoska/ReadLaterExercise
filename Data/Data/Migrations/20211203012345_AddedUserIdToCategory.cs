4e6c3cdbfadf509a091eb6613079d1faee3dc094
s.stojanoska.98 <s.stojanoska.98@gmail.com>
2021-12-03 02:43:28 +0100

    commit

---
+++ Data/Data/Migrations/20211203012345_AddedUserIdToCategory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class AddedUserIdToCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserID",
+                table: "Categories",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserID",
+                table: "Categories");
+        }
+    }
+}
---
+++ ReadLater5/ReadLater5/Controllers/BookmarkController.cs
@@ -0,0 +1,135 @@
+﻿using Entity;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ReadLater5.Controllers
+{
+    [Authorize]
+    public class BookmarkController : Controller
+    {
+        IBookmarkService _bookmarkService;
+        ICategoryService _categoryService;
+        public BookmarkController(IBookmarkService bookmarkService, ICategoryService categoryService)
+        {
+            _bookmarkService = bookmarkService;
+            _categoryService = categoryService;
+        }
+        // GET: Categories
+        public IActionResult Index()
+        {
+            Guid UserId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            List<Bookmark> model = _bookmarkService.GetBookmarks(UserId);
+            return View(model);
+        }
+
+        // GET: Categories/Details/5
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new StatusCodeResult(Microsoft.AspNetCore.Http.StatusCodes.Status400BadRequest);
+            }
+            Bookmark bookmark = _bookmarkService.GetBookmark((int)id);
+            if (bookmark == null)
+            {
+                return new StatusCodeResult(Microsoft.AspNetCore.Http.StatusCodes.Status404NotFound);
+            }
+            return View(bookmark);
+
+        }
+
+        // GET: Categories/Create
+        public IActionResult Create()
+        {
+            BookmarkCreateUpdateView bmCA = new BookmarkCreateUpdateView();
+            bmCA.Bookmark = new Bookmark();
+            Guid UserId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            bmCA.CategoryList = _categoryService.GetCategories(UserId);
+            return View(bmCA);
+        }
+
+        // POST: Categories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Bookmark bookmark)
+        {
+            if (ModelState.IsValid)
+            {
+                bookmark.CreateDate = DateTime.Now;
+                bookmark.UserID = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+                _bookmarkService.CreateBookmark(bookmark);
+                return RedirectToAction("Index");
+            }
+
+            return View(bookmark);
+        }
+
+        // GET: Categories/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new StatusCodeResult(Microsoft.AspNetCore.Http.StatusCodes.Status400BadRequest);
+            }
+            BookmarkCreateUpdateView bmCA = new BookmarkCreateUpdateView();
+            bmCA.Bookmark = _bookmarkService.GetBookmark((int)id);
+            Guid UserId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            bmCA.CategoryList = _categoryService.GetCategories(UserId);
+            if (bmCA.Bookmark == null)
+            {
+                return new StatusCodeResult(Microsoft.AspNetCore.Http.StatusCodes.Status404NotFound);
+            }
+            return View(bmCA.Bookmark);
+        }
+
+        // POST: Categories/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Bookmark bookmark)
+        {
+            if (ModelState.IsValid)
+            {
+                _bookmarkService.UpdateBookmark(bookmark);
+                return RedirectToAction("Index");
+            }
+            return View(bookmark);
+        }
+
+        // GET: Categories/Delete/5
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new StatusCodeResult(Microsoft.AspNetCore.Http.StatusCodes.Status400BadRequest);
+            }
+           Bookmark bookmark = _bookmarkService.GetBookmark((int)id);
+            if (bookmark == null)
+            {
+                return new StatusCodeResult(Microsoft.AspNetCore.Http.StatusCodes.Status404NotFound);
+            }
+            return View(bookmark);
+        }
+
+        // POST: Categories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            Bookmark bookmark = _bookmarkService.GetBookmark(id);
+            _bookmarkService.DeleteBookmark(bookmark);
+            return RedirectToAction("Index");
+        }
+    }
+}
